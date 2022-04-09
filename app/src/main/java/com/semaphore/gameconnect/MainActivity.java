42d94ec2c90dab9256436e1b0718a5384d56c5f7
Marianne Rario <mariannecastillorario27@gmail.com>
2022-04-10 02:10:43 +0800

    add array of winning positions

--- app/src/main/java/com/semaphore/gameconnect/MainActivity.java
+++ app/src/main/java/com/semaphore/gameconnect/MainActivity.java
@@ -11,8 +11,17 @@ public class MainActivity extends AppCompatActivity {
 
     int activePlayer = 0; // this integer will serve as a flag (boolean); 0 - Yellow, 1 - Red
 
-    // initially, the state of the game is none. None represents by 2, so in our array, let's put nine 2s
-    int[] gameState = {}
+    // initially, the state of the game is none. Empty represents by 2, so in our array, let's put nine 2s
+    // 0 - Yellow, 1 - Red, 2 - Empty
+    int[] gameState = {2,2,2,2,2,2,2,2,2};
+
+    // create an array of winning positions
+    int [][] winningPositions = {
+                                {0,1,2},{3,4,5},{6,7,8}, // horizontal winning positions
+                                {0,3,6},{1,4,7},{2,5,8}, // vertical winning positions
+                                {0,4,8}, // diagonal right winning position
+                                {2,4,6} // diagonal left winning position
+                                };
 
     /* onClick functions here */
     public void dropIn(View view){ // need a View parameter which is the ImageView that was tapped on
@@ -21,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
 
         // check the tag
         Log.i("Tag", "Tag: " + counter.getTag());
+        int tappedCounterTag = Integer.parseInt(counter.getTag().toString());
+
+        // game state tracker - change the gameState array every tapped using the imageview tag
+        gameState[tappedCounterTag] = activePlayer;
 
         // animate the counter
         counter.setTranslationY(-1000); // take it off at the top of the screen; san manggagaling
